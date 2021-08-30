c2fff5e3260670400a38e7c495f416c8e68ab572
sabrina <sabrina_lima8@live.com>
2021-08-30 17:46:55 -0300

    a

--- src/app/Ativos/ativos-routing.module.ts
+++ src/app/Ativos/ativos-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from '../_guards';
 import { BuscarAtivosComponent } from './buscar-ativos/buscar-ativos.component';
 import { InserirAtivosComponent } from './inserir-ativos/inserir-ativos.component';
 
 
 const routes: Routes = [
-  {path:'inserir', component:InserirAtivosComponent},
-  {path: 'buscar', component:BuscarAtivosComponent}
+  {path:'inserir', component:InserirAtivosComponent, canActivate: [AuthGuard], canLoad: [AuthGuard]},
+  {path: 'buscar', component:BuscarAtivosComponent, canActivate: [AuthGuard], canLoad: [AuthGuard]}
 ]
 
 @NgModule({
